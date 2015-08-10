9f69f3a9fc3ec0e46324cb5460510bb9c907f78d
zhangxinli <zhangxinli@bupt.edu.cn>
2015-08-10 18:34:50 +0800

    翻转

---
+++ math/Reverse.java
@@ -0,0 +1,12 @@
+package math;
+
+public class Reverse {
+	public int reverse(int x) {
+	       long res =0 ;
+	       while(x!=0){
+	           res= res*10+x%10 ;
+	           x /=10 ;
+	       }
+	        return (res > Integer.MAX_VALUE || res < Integer.MIN_VALUE)? 0 : (int)res;
+	    }
+}
